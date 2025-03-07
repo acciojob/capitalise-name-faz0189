9249fe95fcb5372a1320900f147c466bc7fba18e
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-07 14:48:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+
+const inputfiled = document.getElementbyId('fname')
+
+inputfiled.addEventListener(function (){
+	inputfiled = inputfiledvalue.toUpperCase();
+});
