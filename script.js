35c2c9432f6677bd8908087db155b14dd6d34d57
faz0189 <128984518+faz0189@users.noreply.github.com>
2025-03-07 14:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+<script>
+    // Select the input field by its ID
+    const inputField = document.getElementById('fname');
 
-const inputfiled = document.getElementbyId('fname')
-
-inputfiled.addEventListener(function (){
-	inputfiled.value = inputfiledvalue.toUpperCase();
-});
+    // Add an event listener for when the input loses focus (blur event)
+    inputField.addEventListener('blur', function() {
+        inputField.value = inputField.value.toUpperCase();
+    });
+</script>
